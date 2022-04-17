12d4e1185b7e6b9bdc3a63e153199e55b99a4fdc
boris <boris@home.ru>
2022-04-17 13:47:55 +0800

    add nameof_annotation package

---
+++ packages/nameof_annotation/lib/nameof_annotation.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library nameof_annotation;
+
+export 'src/nameof_annotation_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ packages/nameof_annotation/test/nameof_annotation_test.dart
@@ -0,0 +1,16 @@
+import 'package:nameof_annotation/nameof_annotation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
