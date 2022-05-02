699e5b0ae8f70c5dd0e3c907ec738f49390de602
boris <boris@home.ru>
2022-05-02 20:33:22 +0800

    refactor: remove comments

--- packages/nameof/test/nameof_test.dart
+++ packages/nameof/test/nameof_test.dart
@@ -3,8 +3,6 @@ import 'package:test/test.dart';
 import 'integration/model_one.dart';
 
 void main() {
-  // TODO Использовать source_gen_test
-  // Можно дополнить его тестом build_test
   group('A group of tests for check generation code', () {
     test('test generate code', () {
       expect(nameofModelOne.className, 'ModelOne');
