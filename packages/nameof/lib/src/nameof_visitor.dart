16e0a13ac2900e20deaad38de1caf57f53ae4432
boris <boris@home.ru>
2022-05-05 18:26:45 +0800

    refactor: rename visitor

--- packages/nameof/lib/src/nameof_visitor.dart
+++ packages/nameof/lib/src/nameof_visitor.dart
@@ -1,28 +1,29 @@
 import 'package:analyzer/dart/element/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
 
-class ClassVisitor extends SimpleElementVisitor<void> {
+class NameofVisitor extends SimpleElementVisitor<void> {
   late String className;
+
+  final constructors = <String>{};
   final fields = <String>{};
   final functions = <String>{};
 
-  ClassVisitor(this.className);
+  NameofVisitor(this.className);
 
   @override
   void visitClassElement(ClassElement element) {
     className = element.name;
   }
 
-  // @override
-  // visitConstructorElement(ConstructorElement element) {
-  //   className = element.type.returnType.getDisplayString(withNullability: false);
-  //   return super.visitConstructorElement(element);
-  // }
+  @override
+  visitConstructorElement(ConstructorElement element) {
+    // className = element.type.returnType.getDisplayString(withNullability: false);
+    // return super.visitConstructorElement(element);
+  }
 
   @override
   void visitFieldElement(FieldElement element) {
     // Getters and Setters included
-
     fields.add(element.name);
   }
 
