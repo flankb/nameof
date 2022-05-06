481e0f3f3375ccab3ce1bc7a78693e584ac72d12
boris <boris@home.ru>
2022-05-07 00:35:33 +0800

    refactor: refactor visitor with ElementInfo

--- packages/nameof/lib/src/nameof_visitor.dart
+++ packages/nameof/lib/src/nameof_visitor.dart
@@ -1,12 +1,16 @@
 import 'package:analyzer/dart/element/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:nameof/src/model/element_info.dart';
+import 'package:nameof/src/util/element_extensions.dart';
+import 'package:nameof_annotation/nameof_annotation.dart';
 
 class NameofVisitor extends SimpleElementVisitor<void> {
   late String className;
 
-  final constructors = <String>{};
-  final fields = <String>{};
-  final functions = <String>{};
+  final constructors = <String, ElementInfo>{};
+  final constructorParams = <String, ElementInfo>{};
+  final fields = <String, ElementInfo>{};
+  final functions = <String, ElementInfo>{};
 
   NameofVisitor(this.className);
 
@@ -24,7 +28,7 @@ class NameofVisitor extends SimpleElementVisitor<void> {
   @override
   void visitFieldElement(FieldElement element) {
     // Getters and Setters included
-    fields.add(element.name);
+    fields[element.name] = _getElementInfo(element);
   }
 
   // @override
@@ -34,6 +38,21 @@ class NameofVisitor extends SimpleElementVisitor<void> {
 
   @override
   void visitMethodElement(MethodElement element) {
-    functions.add(element.name);
+    functions[element.name] = _getElementInfo(element);
+  }
+
+  ElementInfo _getElementInfo(ClassMemberElement element) {
+    if (element.name == null) {
+      throw UnsupportedError('Element does not have a name!');
+    }
+
+    final isPrivate = element.name!.startsWith('_');
+
+    String? name = element.hasAnnotation(NameofKey)
+        ? element.getAnnotation(NameofKey)?.getField('name')?.toStringValue() ??
+            element.name
+        : element.name;
+
+    return ElementInfo(name: name!, isPrivate: isPrivate);
   }
 }
