01af737aeb8dbeed16b43a37c1913216dc128ac2
boris <boris@home.ru>
2022-05-07 00:32:55 +0800

    feat: add generator options

---
+++ packages/nameof/lib/src/model/options.dart
@@ -0,0 +1,9 @@
+import 'package:nameof_annotation/nameof_annotation.dart';
+
+/// Generator options
+class NameofOptions {
+  final CoverageBehaviour coverage;
+  final NameofScope scope;
+
+  NameofOptions({required this.coverage, required this.scope});
+}
