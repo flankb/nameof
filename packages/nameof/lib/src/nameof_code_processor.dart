ed84ac863bb5fa566889bcf90f1bb1181faeeced
boris <boris@home.ru>
2022-05-11 16:39:40 +0800

    feat: add NameofCodeProcessor

--- packages/nameof/lib/src/nameof_code_processor.dart
+++ packages/nameof/lib/src/nameof_code_processor.dart
@@ -1,5 +1,73 @@
+import 'package:nameof/src/util/string_extensions.dart';
+import 'package:nameof_annotation/nameof_annotation.dart';
+
+import 'model/element_info.dart';
+import 'model/options.dart';
+import 'model/property_info.dart';
+import 'nameof_visitor.dart';
+
 class NameofCodeProcessor {
+  final NameofOptions options;
+  final NameofVisitor visitor;
+
+  NameofCodeProcessor(this.options, this.visitor);
+
+  String _generateNames(NameofVisitor visitor) {
+    StringBuffer buffer = StringBuffer();
+
+    final classContainerName = '_\$Nameof${visitor.className}';
+
+    buffer.writeln('class $classContainerName {');
+    buffer.writeln('const $classContainerName();');
+
+    final className = 'final String className = \'${visitor.className}\';';
+
+    final constructorNames = getFilteredNames(visitor.constructors.values)
+        .map((e) => e.name)
+        .map((f) =>
+            'final String constructor${f.capitalize().privatize()} = \'$f\';');
+
+    final fieldNames = getFilteredNames(visitor.fields.values)
+        .map((e) => e.name)
+        .map(
+            (f) => 'final String field${f.capitalize().privatize()} = \'$f\';');
+
+    final functionNames = getFilteredNames(visitor.functions.values)
+        .map((e) => e.name)
+        .map((func) =>
+            'final String function${func.capitalize().privatize()} = \'$func\';');
+
+    final propertyNames = getFilteredNames(visitor.properties.values).map((prop) =>
+        'final String property${(prop as PropertyInfo).propertyPrefix}${prop.name.capitalize().privatize()} = \'${prop.name}\';');
+
+    buffer.writeln(className);
+
+    buffer.writeln();
+    buffer.writeln(join(constructorNames));
+
+    buffer.writeln();
+    buffer.writeln(join(fieldNames));
+
+    buffer.writeln();
+    buffer.writeln(join(propertyNames));
+
+    buffer.writeln();
+    buffer.writeln(join(functionNames));
+
+    buffer.writeln('}');
+
+    buffer.writeln('const nameof${visitor.className} = $classContainerName();');
+
+    return buffer.toString();
+  }
+
+  Iterable<ElementInfo> getFilteredNames(Iterable<ElementInfo> infos) {
+    return options.coverage == CoverageBehaviour.includeImplicit
+        ? infos.map((e) => e)
+        : infos.where((element) => element.isAnnotated).map((e) => e);
+  }
+
   String process() {
-    return '';
+    return _generateNames(visitor);
   }
 }
--- packages/nameof/lib/src/nameof_generator.dart
+++ packages/nameof/lib/src/nameof_generator.dart
@@ -1,10 +1,8 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
-import 'package:nameof/src/util/element_extensions.dart';
-import 'package:nameof/src/util/string_extensions.dart';
+import 'package:nameof/src/nameof_code_processor.dart';
 import 'package:nameof_annotation/nameof_annotation.dart';
 import 'package:source_gen/source_gen.dart';
-
 import 'model/options.dart';
 import 'nameof_visitor.dart';
 
@@ -16,58 +14,25 @@ class NameofGenerator extends GeneratorForAnnotation<Nameof> {
       throw UnsupportedError("This is not a class!");
     }
 
-    /// Получить значение аннотации
-    final behaviour = CoverageBehaviour.values.firstWhere((element) =>
-        element.toString() ==
-        annotation.objectValue.getField('coverageBehaviour')?.toStringValue());
+    final behaviour = enumValueForDartObject(
+      annotation.read('coverageBehaviour').objectValue,
+      CoverageBehaviour.values,
+    );
 
     final options =
         NameofOptions(coverage: behaviour, scope: NameofScope.onlyPublic);
 
-    //annotation.objectValue.getField('coverageBehaviour')?.toStringValue();
-    //final behaviorValue = element.getAnnotation(Nameof)?.getField('behaviour')?.toStringValue();
-
-    // По базовым классам можно тоже пройтись визитором
-
-    // final annotatedElements =
-    //     (element as ClassElement).fields.where((f) => f.hasAnnotation(Nameof));
-    //=============
-
     final visitor = NameofVisitor(element.name ?? 'NoName');
     element.visitChildren(visitor);
-    final code = _generateNames(visitor);
+
+    final code = NameofCodeProcessor(options, visitor).process();
 
     return code;
   }
 
-  String _generateNames(NameofVisitor visitor) {
-    StringBuffer buffer = StringBuffer();
-
-    final classContainerName = '_\$Nameof${visitor.className}';
-
-    buffer.writeln('class $classContainerName {');
-    buffer.writeln('const $classContainerName();');
-
-    final className = 'final String className = \'${visitor.className}\';';
-
-    final fieldNames = visitor.fields.values
-        .map((e) => e.name)
-        .map((f) => 'final String field${f.capitalize()} = \'$f\';');
-
-    final functionNames = visitor.functions.values.map((e) => e.name).map(
-        (func) =>
-            'final String function${func.capitalize().privatize()} = \'$func\';');
-
-    buffer.writeln(className);
-    buffer.writeln();
-    buffer.writeln(join(fieldNames));
-    buffer.writeln();
-    buffer.writeln(join(functionNames));
-
-    buffer.writeln('}');
-
-    buffer.writeln('const nameof${visitor.className} = $classContainerName();');
-
-    return buffer.toString();
-  }
+  T enumValueForDartObject<T>(
+    dynamic source,
+    List<T> items,
+  ) =>
+      items[source.getField('index')!.toIntValue()!];
 }
